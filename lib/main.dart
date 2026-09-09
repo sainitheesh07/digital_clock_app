bdf88bcedce0009bb0699d89be24cfe1f0c70808
gnvsainitheesh <gnvsainitheesh@gmail.com>
2026-09-09 23:09:05 +0530

    Add real-time clock functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,18 +10,61 @@ class DigitalClockApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            '00:00:00',
-            style: TextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
-              color: Colors.greenAccent,
-            ),
+      home: DigitalClockScreen(),
+    );
+  }
+}
+
+class DigitalClockScreen extends StatefulWidget {
+  const DigitalClockScreen({super.key});
+
+  @override
+  State<DigitalClockScreen> createState() => _DigitalClockScreenState();
+}
+
+class _DigitalClockScreenState extends State<DigitalClockScreen> {
+  DateTime currentTime = DateTime.now();
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        currentTime = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  String formatTime(int value) {
+    return value.toString().padLeft(2, '0');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String time =
+        '${formatTime(currentTime.hour)}:'
+        '${formatTime(currentTime.minute)}:'
+        '${formatTime(currentTime.second)}';
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          time,
+          style: const TextStyle(
+            fontSize: 60,
+            fontWeight: FontWeight.bold,
+            color: Colors.greenAccent,
           ),
         ),
       ),
